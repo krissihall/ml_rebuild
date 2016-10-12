5b5452cc4c8cec442374fb958658e94c52abba60
Hall, Kristina <HallK@AE.com>
2016-10-12 09:41:50 -0400

    master: backup all changes

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,8 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -20,5 +22,31 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  app.import('bower_components/jquery/dist/jquery.js');
+
+  app.import('bower_components/tether/dist/js/tether.js');
+  app.import('bower_components/tether/dist/css/tether.css');
+
+  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+  var bootstrapPath = 'bower_components/bootstrap/dist/';
+
+  var fontAssets = new Funnel(bootstrapPath, {
+      srcDir: 'fonts',
+      destDir: '/vendor/bootstrap/fonts'
+  });
+
+  var cssAssets = new Funnel(bootstrapPath, {
+      srcDir: 'css',
+      destDir: '/vendor/bootstrap/css'
+  });
+
+  // var imageAssets = new Funnel(bootstrapPath, {
+  //     srcDir: 'images',
+  //     destDir: '/vendor/bootstrap/images'
+  // });
+
+  return mergeTrees([app.toTree(), fontAssets, cssAssets]);
+
+  // return app.toTree();
 };
